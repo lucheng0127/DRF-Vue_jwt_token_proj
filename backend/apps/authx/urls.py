5d594250fcd2036174a75ca0b98f2480cede5e45
鲁成 <lc960127@gmail.com>
2017-11-14 09:44:45 +0800

    add vue frontend

--- backend/apps/authx/urls.py
+++ backend/apps/authx/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
