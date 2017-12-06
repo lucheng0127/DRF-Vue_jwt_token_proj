9d4f2b92a7e7449a529b59e20f0f72d5c1d6d20b
鲁成 <lc960127@gmail.com>
2017-12-06 16:44:38 +0800

    add set_password

--- backend/apps/authx/views.py
+++ backend/apps/authx/views.py
@@ -5,10 +5,11 @@ import re
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import detail_route
 
 from django.contrib.auth.models import User
 
-from apps.authx.serializers import UserSerializer, UserCreateSerializer
+from apps.authx.serializers import UserSerializer, UserCreateSerializer, PasswordSerializer
 from apps.authx.permissions import IsSuperuserOrOwnerOrReadOnly
 
 
@@ -37,3 +38,18 @@ class UserViewSet(viewsets.ModelViewSet):
             User.objects.create_user(**serializer.data)
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @detail_route(methods=['POST'])
+    def set_passwd(self, request, pk=None):
+        user = User.objects.get(pk=pk)
+        if not (user == request.user or request.user.is_superuser):
+            return Response({'msg': u'只有本人和管理员可以修改密码'}, status=status.HTTP_403_FORBIDDEN)
+        data = request.data
+        if len(data['password']) < 8:
+            return Response({'msg': u'密码至少8位'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user.set_password(data['password'])
+            user.save()
+            return Response({'msg': u'设置密码成功'}, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
