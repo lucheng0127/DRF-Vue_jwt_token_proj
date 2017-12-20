cefd59d8c075acef5dca95a82e7b6f952d340d85
鲁成 <lc960127@gmail.com>
2017-12-20 13:13:21 +0800

    add import thesis page without add http action

--- frontend/src/components/thesis/ImportThesis.vue
+++ frontend/src/components/thesis/ImportThesis.vue
@@ -9,7 +9,16 @@
     </div>
     <div class="layout-content">
       <div class="layout-content-main">
-        Import Thesis Page
+        <h3>批量导入毕业设计数据</h3>
+        <b>通信工程：</b>CE<br/><b>信息与计算科学：</b>IS<br>
+        [学生姓名1],[学生学号1],[专业1],[毕业年份1],[论文题目1]<br>
+        [学生姓名2],[学生学号2],[专业2],[毕业年份2],[论文题目2]<br>
+        <p class="notify-msg">逗号为英文状态下逗号','</p>
+        <Alert type="warning" show-icon>总数据{{all_data}}条</Alert>
+        <Alert type="success" show-icon>成功{{good_data}}条</Alert>
+        <Alert type="error" show-icon>失败{{bad_data}}条</Alert>
+        <Input v-model="data" type="textarea" :rows="10" placeholder="鲁成,20140154004,CE,2018,学院材料收集系统"></Input>
+        <Button type="primary" shape="circle" icon="ios-download-outline">导入</Button>
         <router-view/>
       </div>
     </div>
@@ -18,7 +27,18 @@
 
 <script>
   export default {
-    name: 'import-thesis'
+    name: 'import-thesis',
+    data () {
+      return {
+        good_data: 0,
+        bad_data: 0
+      }
+    },
+    computed: {
+      all_data: function () {
+        return this.good_data + this.bad_data
+      }
+    }
   }
 </script>
 
@@ -39,5 +59,12 @@
   .layout-content-main{
     height: 100%;
     padding: 10px;
+  }
+  .layout-content-main button{
+    float: right;
+    margin-top: 5px;
+  }
+  .notify-msg{
+    color: red;
   }
 </style>
